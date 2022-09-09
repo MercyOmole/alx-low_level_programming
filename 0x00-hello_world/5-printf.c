d73507b9d775e86e0cdcceefe918222d8e26a77d
Mercy <mercyomole229@gmail.com>
2022-09-09 10:42:10 -0700

    correct 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,9 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
