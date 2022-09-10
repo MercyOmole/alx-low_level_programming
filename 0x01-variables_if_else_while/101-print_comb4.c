f75aebd85229f8a9bf94cd65699f362484c05c1f
Mercy <mercyomole229@gmail.com>
2022-09-09 19:39:37 -0700

    Correct 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,9 +23,9 @@ int main(void)
 			while (digit3 <= 9)
 			{
 				if (digit1 != digit2 && 
-					digit1 < digit2 && 
-					digit2 != digit3 && 
-					digit2 < digit3)
+						digit1 < digit2 &&
+					       	digit2 != digit3 && 
+						digit2 < digit3)
 				{
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
