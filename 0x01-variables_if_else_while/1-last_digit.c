00d00fcda3741dd74eaab86e8ba8f98c1ac20314
Mercy <mercyomole229@gmail.com>
2022-09-09 17:41:39 -0700

    correct 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
  * main - Entry point
  *
  * Description: print the value of n status:
- * 	greater than, is zero and is not less than 6.
+ * greater than, is zero and is not less than 6.
  *
  * Return: Always O (Success)
 */
