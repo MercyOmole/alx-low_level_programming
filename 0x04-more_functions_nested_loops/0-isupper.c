37564f5eba863eed244cee6373923343fcb7c716
Mercy <mercyomole229@gmail.com>
2022-09-16 16:44:46 -0700

    Edit files

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - check if c is upper
  *
  * @c: input for alphabet
  *
- * Return: 1 if it is uppercase, o if not
+ * Return: 1 if it is uppercase, 0 if not
  */
 
 int _isupper(int c)
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,3 +1,6 @@
+#include"main.h"
+#include <stdio.h>
+
 /**
  * _mul - multiplies two integers
  *
