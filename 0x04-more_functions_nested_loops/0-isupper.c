0938b54e6c74348dd188afa247e561da3398830f
Mercy <mercyomole229@gmail.com>
2022-09-16 16:56:49 -0700

    Edit 0-isupper.c file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _isupper - check if c is upper
@@ -15,8 +14,6 @@ int _isupper(int c)
 	{
 		return (1);
 	}
-	else
-	{
-		return (0);
-	}
+	
+	return (0);
 }
