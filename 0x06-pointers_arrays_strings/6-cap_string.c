7f16cf237f03ecea8e2f4d562e929cb3eb31af34
Mercy <mercyomole229@gmail.com>
2022-09-24 04:51:11 -0700

    Write a function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,47 +1,52 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * cap_string - a function that capitalizes
  * all words of a string
- *
  * @s: pointer to char input array
- *
  * Return: @s
  */
 
 char *cap_string(char *s)
 {
-	int i = 0;
+	int index = 0;
 
-	/*iterate through our array values*/
-	while (s[i] != '\0')
+	while (str[++index])
 	{
-		/*check for any lowercase letters*/
-		if (s[i] >= 97 && s[i] <= 122)
-		{
-			/**
-			 * if we have a null character
-			 * change its value to capital
-			 */
-			if (i == 0)
-			{
-				s[i] -= 32;
-			}
-			/**
-			 * if we find any character matching the below
-			 * before any small letter, we change that value
-			 * to a capital letter.
-			 */
-			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
-				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
-				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
-				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
-				s[i - 1] == 124)
-			{
-				s[i] -= 32;
-			}
-		}
-		i++
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}')
+			str[index] -= 32;
 	}
-	return (s);
+
+	return (str);
 }
