a3cca41556f13d8744d0a47d80261dccfc85ffd9
Mercy <mercyomole229@gmail.com>
2022-10-02 10:39:35 -0700

    Write a program that prints the minimum number of coins to make change for an amount of money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -27,16 +27,12 @@ int main(int args, char *argv[])
 	{
 		if (c >= 25)
 			c -= 25;
-
 		else if (c >= 10)
 			c -= 10;
-
 		else if (c >= 5)
 			c -= 5;
-
 		else if (c >= 2)
-			c -= 2
-
+			c -= 2;
 		else if (c >= 1)
 			c -= 1;
 		else
